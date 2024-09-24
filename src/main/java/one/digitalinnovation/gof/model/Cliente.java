c24b6d96d04e03f54a76c7fca35b65bd7e05fd34
gustavo.ribeiro <gustavo.ribeiro@sc.senai.br>
2024-09-23 21:39:14 -0300

    Design Patterns com Spring

---
+++ src/main/java/one/digitalinnovation/gof/model/Cliente.java
@@ -0,0 +1,44 @@
+package one.digitalinnovation.gof.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long idCliente;
+
+    private String nome;
+
+    @ManyToOne
+    private Endereco endereco;
+
+    public Long getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Long idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+}
